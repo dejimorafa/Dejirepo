5df5ea6c1f9bdf8b927f52dd58eb4e0fe931680e
deji <dejimorafa@gmail.com>
2023-04-15 23:02:40 -0400

    a change on a branch

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Liverpool')
+    print_hi('Liverpool is the best team')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
